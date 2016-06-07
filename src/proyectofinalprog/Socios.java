2e2f24dcf4a426f7f5b7f15759a0bb221b35adc4
Nabor <Nabor@DESKTOP-BTR17DU.home>
2016-06-07 21:31:28 +0200

    JFrame que nos permite el logueo de Socios. En caso de ser correcta la información desplega un panel de opciones.

--- src/proyectofinalprog/Socios.java
+++ src/proyectofinalprog/Socios.java
@@ -5,6 +5,13 @@
  */
 package proyectofinalprog;
 import DataBaseLibrary.Metodos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.FocusManager;
 import javax.swing.JOptionPane;
 
@@ -17,17 +24,19 @@ public class Socios extends javax.swing.JFrame {
     /**
      * Creates new form Socios
      */
+    com.mysql.jdbc.Connection conn=null;
+    static String sql="";  
     
-    static String sql;  
-    Metodos library = new Metodos();
+   
     public Socios() {
                                             
-
+        Metodos library = new Metodos();
         initComponents();
         jPanel1.setVisible(false);
-        library.Conectar("jdbc:mysql://localhost", "zoo", "user=root", "password=Sobrada12345");
-        buscar("contraseña", "socios", "idsocio", "1");
-         passField.getText().toString();
+        Conectar("jdbc:mysql://localhost", "zoo","user=root","password=Sobrada12345");
+        
+       
+        
         
         
         
@@ -46,6 +55,9 @@ public class Socios extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
         registrarButton = new javax.swing.JButton();
         usuarioField = new javax.swing.JTextField();
         connButton = new javax.swing.JButton();
@@ -53,6 +65,7 @@ public class Socios extends javax.swing.JFrame {
         bajaButton = new javax.swing.JButton();
         modificarButton = new javax.swing.JButton();
         passField = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
 
         jButton2.setText("jButton2");
 
@@ -61,6 +74,12 @@ public class Socios extends javax.swing.JFrame {
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
+
+        jButton1.setText("jButton1");
+
+        jButton4.setText("jButton4");
+
+        jButton5.setText("jButton5");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -103,14 +122,20 @@ public class Socios extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
                 .addComponent(bajaButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(modificarButton)
-                .addContainerGap(265, Short.MAX_VALUE))
+                .addContainerGap(273, Short.MAX_VALUE))
         );
 
         passField.setText("Contraseña");
+
+        jButton3.setText("Desconectar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -120,33 +145,34 @@ public class Socios extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(47, 47, 47)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(usuarioField)
-                            .addComponent(passField, javax.swing.GroupLayout.DEFAULT_SIZE, 87, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(usuarioField, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(passField, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(41, 41, 41)
-                        .addComponent(connButton)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(connButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(92, 92, 92)
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(193, 193, 193)
                         .addComponent(registrarButton)))
-                .addContainerGap(44, Short.MAX_VALUE))
+                .addContainerGap(33, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(24, 24, 24)
-                        .addComponent(connButton))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(usuarioField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(passField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(usuarioField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(connButton))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton3)
+                            .addComponent(passField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
                 .addComponent(registrarButton)
                 .addContainerGap())
@@ -156,7 +182,9 @@ public class Socios extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void registrarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registrarButtonActionPerformed
-        // TODO add your handling code here:
+        Suscribirse obxS = new Suscribirse();
+        obxS.setVisible(true);
+        Socios.this.setVisible(false);
     }//GEN-LAST:event_registrarButtonActionPerformed
 
     private void bajaButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bajaButtonActionPerformed
@@ -164,16 +192,20 @@ public class Socios extends javax.swing.JFrame {
     }//GEN-LAST:event_bajaButtonActionPerformed
 
     private void connButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_connButtonActionPerformed
-        System.out.println(sql);
-        System.out.println(passField.getText());
-        
-        if(passField.getText().compareToIgnoreCase(sql)==0){
+        buscar("contraseña", "socios", "idsocio",usuarioField.getText());
+        if(passField.getText().equals(sql)){
         jPanel1.setVisible(true);
         }
         else{
             JOptionPane.showMessageDialog(null,"Contraseña Incorrecta!");
+            sql="";
         }
     }//GEN-LAST:event_connButtonActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        jPanel1.setVisible(false);
+        sql="";
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -184,7 +216,11 @@ public class Socios extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bajaButton;
     private javax.swing.JButton connButton;
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
@@ -194,17 +230,43 @@ public class Socios extends javax.swing.JFrame {
     private javax.swing.JButton registrarButton;
     private javax.swing.JTextField usuarioField;
     // End of variables declaration//GEN-END:variables
- public String buscar(String parametro,String nomTabla,String ID,String primaryKey){
+ public void buscar(String parametro,String nomTabla,String ID,String valores) {
+       
+      
+       try {
+            Statement st= conn.createStatement();
+               ResultSet rs= st.executeQuery("Select "+parametro+" from "+nomTabla+" where "+ID+"='"+valores+"'");
+                    String [] datos= new String[6];
+          while(rs.next()){
+          
+            for (int i = 0; i < datos.length; i++) {
+                datos[i]=rs.getString(i+1);
+                 sql= sql +""+datos[i];
+                
+            }
+           
+              }
+                   
+        } catch (SQLException ex) {
+            System.out.println("Error en la visualizacion "+ex.getMessage());
+        }
         
-        
-            sql="Select "+parametro+" from "+nomTabla+" where "+ID+"='"+primaryKey+"'";
-            
-            return sql;
-            
-            
-        
+ }
+
+        public Connection Conectar(String servidor,String base,String usuario,String contraseña){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conn =(com.mysql.jdbc.Connection) DriverManager.getConnection(servidor+"/"+base+"?"+usuario+"&"+contraseña);
+        }catch(Exception conectar){
+            System.out.println(conectar.getMessage());
+        }
+        return conn; 
     }
-
-
 }
+            
+        
+    
+
+
+
 
